608922969cfd3e70665fa7b807051c821aa22ca9
conorspicer <spicer.conor@gmail.com>
2017-10-09 21:55:38 +0100

    with export script

---
+++ scripts/output_picks.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from picks.models import Pick
+from fixtures.models import Fixture
+from django_pandas.io import read_frame
+qs = Pick.objects.all()
+df = read_frame(qs)
+qs.to_dataframe(index='id')
+
+pd.DataFrame(list(Fixture.objects.all().values())).to_csv('fixtures_output.csv')
+pd.DataFrame(list(Pick.objects.filter(user__username="magnusmartinsen").values())).to_csv('picks_output.csv')
+
+picks = pd.DataFrame(list(Pick.objects.filter(user__username="magnusmartinsen").values()))
+fixtures = pd.DataFrame(list(Fixture.objects.all().values()))
+picks.set_index('fixture_id').join(fixtures.set_index('id')).to_csv('magnus.csv')
