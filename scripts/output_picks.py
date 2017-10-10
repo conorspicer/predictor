f41bca8b2e4a864384aad81a62bd60d72708bd19
conorspicer <spicer.conor@gmail.com>
2017-10-10 22:22:57 +0100

    updated export statements

--- scripts/output_picks.py
+++ scripts/output_picks.py
@@ -9,6 +9,6 @@ qs.to_dataframe(index='id')
 pd.DataFrame(list(Fixture.objects.all().values())).to_csv('fixtures_output.csv')
 pd.DataFrame(list(Pick.objects.filter(user__username="magnusmartinsen").values())).to_csv('picks_output.csv')
 
-picks = pd.DataFrame(list(Pick.objects.filter(user__username="magnusmartinsen").values()))
+picks = pd.DataFrame(list(Pick.objects.filter(user__username="torinmehmet").values()))
 fixtures = pd.DataFrame(list(Fixture.objects.all().values()))
-picks.set_index('fixture_id').join(fixtures.set_index('id')).to_csv('magnus.csv')
+picks.set_index('fixture_id').join(fixtures.set_index('id')).to_csv('mehmet.csv')
