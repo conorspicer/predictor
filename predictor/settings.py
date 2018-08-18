9c6a7bec7af5620a30c80a81a3a4b768ff925f3c
conor.spicer1 <conor.spicer@nationalgrid.com>
2018-08-18 18:52:34 +0100

    With DB name

--- predictor/settings.py
+++ predictor/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'predictor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
+        'NAME': 'nflpredictor$predictorDB',
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
