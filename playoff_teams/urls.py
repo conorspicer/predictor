6245dec1c2f7da827c384a6604b6d64be666357d
conor.spicer1 <conor.spicer@nationalgrid.com>
2018-09-16 12:15:08 +0100

    instructions for re-enabling playoff picks

--- playoff_teams/urls.py
+++ playoff_teams/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     url(r'^$', PlayoffPickList.as_view(), name='all'),
     # url(r'^update/$', PlayoffPickUpdate.as_view(), name='update'),
 ]
+
+# add this back above final div in playoffpick_list.html to re-enable playoff updates & uncomment above
+# <!--<a class="btn btn-success" style="width:200px;" href="{% url 'playoff_teams:update' %}">UPDATE</a>-->
