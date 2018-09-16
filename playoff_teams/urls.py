28552d1b5ebdaf05a26c9b336ede0066755d90f8
conor.spicer1 <conor.spicer@nationalgrid.com>
2018-09-16 12:12:09 +0100

    remove update url for playoff teams

--- playoff_teams/urls.py
+++ playoff_teams/urls.py
@@ -5,5 +5,5 @@ app_name = 'playoff_teams'
 
 urlpatterns = [
     url(r'^$', PlayoffPickList.as_view(), name='all'),
-    url(r'^update/$', PlayoffPickUpdate.as_view(), name='update'),
+    # url(r'^update/$', PlayoffPickUpdate.as_view(), name='update'),
 ]
