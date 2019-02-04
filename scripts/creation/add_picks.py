7b3e646b4ef12fe16b6762a4d3c1bc4e26240b26
conor.spicer1 <conor.spicer@nationalgrid.com>
2019-02-04 18:38:47 +0000

    Script just for this week

--- scripts/creation/add_picks.py
+++ scripts/creation/add_picks.py
@@ -3,7 +3,7 @@ Run this using:
 
 webapp summary screen > launch console with this venv
 cd predictor
-python manage.py shell < scripts/add_picks.py
+python manage.py shell < scripts/creation/add_picks.py
 
 """
 
@@ -12,17 +12,18 @@ from fixtures.models import Fixture
 from picks.models import Pick
 from scripts.get_week import get_week
 
-for person in User.objects.all():
-    for game in Fixture.objects.all():
-        new_entry = Pick(fixture=game, user=person)
-        new_entry.save()
-
-# Add picks for playoff weeks:
-# this_week = get_week()
+# Inital setup for season
 # for person in User.objects.all():
-#     for game in Fixture.objects.filer(week=this_week):
+#     for game in Fixture.objects.all():
 #         new_entry = Pick(fixture=game, user=person)
 #         new_entry.save()
+
+# Add picks for playoff weeks:
+this_week = get_week()
+for person in User.objects.all():
+    for game in Fixture.objects.filer(week=this_week):
+        new_entry = Pick(fixture=game, user=person)
+        new_entry.save()
 
 counter = 0
 for person in User.objects.all():
