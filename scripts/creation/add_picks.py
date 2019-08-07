486062d8ebd4eb2aef4d31e2200c7e8cfc9c8332
conor.spicer1 <conor.spicer@nationalgrid.com>
2019-08-07 21:22:19 +0100

    Script to add user picks for season

--- scripts/creation/add_picks.py
+++ scripts/creation/add_picks.py
@@ -13,19 +13,14 @@ from picks.models import Pick
 from scripts.get_week import get_week
 
 # Inital setup for season
-# for person in User.objects.all():
-#     for game in Fixture.objects.all():
-#         new_entry = Pick(fixture=game, user=person)
-#         new_entry.save()
-
-# Add picks for playoff weeks:
-this_week = get_week()
 for person in User.objects.all():
-    for game in Fixture.objects.filer(week=this_week):
+    for game in Fixture.objects.filter(changeable=1):
         new_entry = Pick(fixture=game, user=person)
         new_entry.save()
 
-counter = 0
-for person in User.objects.all():
-    counter += 1
-print(counter)
+# Add picks for playoff weeks:
+# this_week = get_week()
+# for person in User.objects.all():
+#     for game in Fixture.objects.filer(week=this_week):
+#         new_entry = Pick(fixture=game, user=person)
+#         new_entry.save()
