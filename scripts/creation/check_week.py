3fae61b61452f97f71185d595e6a1cf162b0e1a4
Conor Spicer <conor.spicer1@uk.ibm.com>
2021-01-07 14:25:02 +0000

    date filter for check week

--- scripts/creation/check_week.py
+++ scripts/creation/check_week.py
@@ -17,6 +17,6 @@ current_week = get_week()
 
 print('Current week is {week}, number of games this week {games}'.format(
     week=current_week,
-    games=len(Fixture.objects.filter(week=current_week, ko_datetime__gt=datetime.now() - timedelta(months=12)))
+    games=len(Fixture.objects.filter(week=current_week, ko_datetime__gt=datetime.now() - timedelta(weeks=52)))
 ))
 
