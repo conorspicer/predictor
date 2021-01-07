ff51bbe34e3cfdf2652e8cefeb6de4989386c9c3
Conor Spicer <conor.spicer1@uk.ibm.com>
2021-01-07 14:22:39 +0000

    date filter for check week

--- scripts/creation/check_week.py
+++ scripts/creation/check_week.py
@@ -11,11 +11,12 @@ django.setup()
 
 from fixtures.models import Fixture
 from scripts.get_week import get_week
+from datetime import datetime, timedelta
 
 current_week = get_week()
 
 print('Current week is {week}, number of games this week {games}'.format(
     week=current_week,
-    games=len(Fixture.objects.filter(week=current_week))
+    games=len(Fixture.objects.filter(week=current_week, ko_datetime__gt=datetime.now() - timedelta(months=12)))
 ))
 
