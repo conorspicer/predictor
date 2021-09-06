c8b5e1b1b2da695c7e9386ed477db341329dbf31
Conor Spicer <conor.spicer1@uk.ibm.com>
2021-09-06 16:17:11 +0100

    reset playoff scores for 2021

--- results/models.py
+++ results/models.py
@@ -28,13 +28,13 @@ class UserWeekResult(models.Model):
 
         # Add manual adjustment for playoff scores
         elif self.user.username == 'conorspicer':
-            counter = 500 + 50
+            counter = 0
         elif self.user.username == 'torinmehmet':
-            counter = 400 + 50
+            counter = 0
         elif self.user.username == 'magnusmartinsen':
-            counter = 450
+            counter = 0
         elif self.user.username == 'lewismead':
-            counter = 400
+            counter = 0
         return counter
     user_points = property(_user_points)
 
