9bcd784063e3fff367326eceef2eefd161350fdc
conor.spicer <spicer.conor@gmail.com>
2022-01-21 21:39:27 +0000

    only add current seasons playoff picks

--- scripts/creation/add_playoff_pick_objects_to_db.py
+++ scripts/creation/add_playoff_pick_objects_to_db.py
@@ -7,6 +7,7 @@ python manage.py shell < scripts/creation/add_playoff_pick_objects_to_db.py
 
 """
 import django
+
 django.setup()
 
 from django.contrib.auth.models import User
@@ -15,13 +16,16 @@ from picks.models import Pick
 from scripts.get_week import get_week
 from datetime import datetime, timedelta, timezone
 
-
 current_week = get_week()
 
 for person in User.objects.all():
-    for game in Fixture.objects.filter(week=current_week, ko_datetime__gt=datetime.now(timezone.utc) - timedelta(weeks=52)):
+    for game in Fixture.objects.filter(
+            week=current_week,
+            ko_datetime__gt=datetime.now(timezone.utc) - timedelta(weeks=30)
+    ):
         new_entry = Pick(
             fixture=game,
             user=person,
+
         )
         new_entry.save()
