fa13418c70361923ff690c14a818ca2e0330b8af
conor.spicer <spicer.conor@gmail.com>
2022-09-08 23:24:46 -0400

    set playoff pick scores to 0

--- results/models.py
+++ results/models.py
@@ -28,13 +28,13 @@ class UserWeekResult(models.Model):
 
         # Add manual adjustment for playoff scores
         elif self.user.username == 'conorspicer':
-            counter = 450
+            counter = 0
         elif self.user.username == 'torinmehmet':
-            counter = 450 + 75
+            counter = 0
         elif self.user.username == 'magnusmartinsen':
-            counter = 450
+            counter = 0
         elif self.user.username == 'lewismead':
-            counter = 500 + 75
+            counter = 0
         else:
             counter = 0
         return counter
